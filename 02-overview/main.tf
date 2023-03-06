b96893d2ccf4cd21be5845291eefa77ccb1ed355
sunnykunju <126974554+sunnykunju@users.noreply.github.com>
2023-03-06 16:18:00 -0500

    Update for AMI of us-east-1
    Also ensure Security Group added to the user

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -12,6 +12,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  ami           = "ami-006dcf34c09e50022" 
   instance_type = "t2.micro"
 }
